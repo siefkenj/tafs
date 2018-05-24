a96d931ede948964d2fa05ad534f47016bb098a9
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-05-24 11:30:58 -0400

    Webpack initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<style>
+
+@import './css/app.css';
+
+</style>
+
+<template>
+
+<div id="app">
+    <div id="app">
+        <div id="page">
+            <h1>app</h1>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: "app",
+};
+
+</script>
