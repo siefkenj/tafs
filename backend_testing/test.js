baa504eb5a6857cfc37ecd57c1e66e17cf3ebc2c
Darren <18633059886@163.com>
2018-05-30 15:14:58 -0400

    Set up the structure for the API PHP testing for TA_Feedback

---
+++ backend_testing/test.js
@@ -0,0 +1,129 @@
+// This is the mocha testing file for test.php
+import "isomorphic-fetch";
+import chai from "chai";
+
+describe("Test GET request to API", function () {
+    it("get the list of users", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=users%2F")
+        .catch(function (err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get a user with specific utorid", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=users%2Fabcd1000")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get the list of courses", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=courses%2F")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get a specific course", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=courses%2FCSC411")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get a list of sections of a course", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=courses%2FCSC411%2Fsections")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get a specific section in a course", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=courses%2FCSC411%2Fsections%2FLEC0101")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get the list of surveys", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=survey%2F")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get a specific survey model", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=survey%2F0")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("get reponses from a specific survey model", async function () {
+        let fetched = await fetch("localhost:3000/test.php?url=survey%2F0%2Fresponses")
+        .catch(function(err) {
+            throw(err);
+        });
+        // TODO: Use the expect statement to test the fetched data
+    });
+});
+
+
+describe("Test POST request to API", function () {
+
+    it("create a course", async function () {
+        let fetched = fetch("localhost:3000/test.php", {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify()
+        }).catch(function (err) {
+            throw(err);
+        });
+
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("create a section", async function () {
+        let fetched = fetch("localhost:3000/test.php", {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify()
+        }).catch(function (err) {
+            throw(err);
+        });
+
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("create a survey", async function () {
+        let fetched = fetch("localhost:3000/test.php", {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify()
+        }).catch(function (err) {
+            throw(err);
+        });
+
+        // TODO: Use the expect statement to test the fetched data
+    });
+
+    it("create a response", async function () {
+        let fetched = fetch("localhost:3000/test.php", {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify()
+        }).catch(function (err) {
+            throw(err);
+        });
+
+        // TODO: Use the expect statement to test the fetched data
+    });
+});
