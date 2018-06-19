bbb8b06d34a2e51dd4f364e31202cfc451367fe6
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-06-19 13:28:39 -0400

    Implemented error component

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,37 @@
 <div id="app">
     <div>
         <sidenav></sidenav>
+        <div v-if=this.error>
+            <ErrorBlock :error_message="this.error_message"></ErrorBlock>
+        </div>
         <h1>app</h1>
-        <router-view></router-view>
+        <router-view @error="sendError"></router-view>
     </div>
 </div>
 
 </template>
 
 <script>
-import Sidenav from './views/components/navbar.vue'
+
+import Sidenav from './views/components/navbar.vue';
+import ErrorBlock from './views/components/errorBlock.vue';
 export default {
     name: "app",
-    components:{
-        Sidenav
+    methods: {
+        sendError: function(value) {
+            this.error_message = value;
+            this.error = true;
+        }
+    },
+    data: function() {
+        return {
+            error: false,
+            error_message: ""
+        }
+    },
+    components: {
+        Sidenav,
+        ErrorBlock
     }
 };
 
---
+++ src/views/components/errorBlock.vue
@@ -0,0 +1,30 @@
+<style>
+
+#error-block {
+    background-color: #F23A3A;
+    color: white;
+    text-align: center;
+    border-radius: 10px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    margin-top: 5px;
+}
+
+</style>
+
+<template>
+
+<div id="error-block">
+    <p>Error: {{error_message}}</p>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: "edit-courses",
+    props: ["error_message"]
+};
+
+</script>
