178a4f59b572cce49996290129b2f94dd20d9051
Daniel Chen <chen.daniel2099@gmail.com>
2018-06-19 13:53:35 -0400

    refactored php script to allow mutliple queries, completed survey and survey results api call.

--- get_info.php
+++ get_info.php
@@ -1,7 +1,6 @@
 <?php
-require 'db/config.php';
 require 'get_query_generators.php';
-
+header("Content-type: application/json");
 try {
     $method = "";
     if (isset($_SERVER['REQUEST_METHOD'])) {
@@ -11,11 +10,12 @@ try {
         $data = json_decode(file_get_contents("php://input"), true);
         switch ($method) {
             case "GET":
-                $query = handle_get($_GET);
+                print json_encode(handle_get($_GET));
                 break;
 
             default:
                 throw new Exception("Invalid Request");
+                break;
         }
     } else {
         $error = 'No Request Method Found';
@@ -33,42 +33,40 @@ try {
     exit();
 }
 
-// Attempt to execute sql command and print response in json format.
-// If a sql error occurs, a JSON error object is returned.
-try {
-    // connect to the mysql database
-    $conn = new PDO(
-        "mysql:host=$servername;dbname=$database",
-        $username,
-        $password
-    );
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare($query);
+function get_query_result($query_string, $bind_variables)
+{
+    require '../db/config.php';
+    // Attempt to execute sql command and print response in json format.
+    // If a sql error occurs, JSON error object.
+    try {
+        // connect to the mysql database
+        $conn = new PDO(
+            "mysql:host=$servername;dbname=$database",
+            $username,
+            $password
+        );
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare($query_string);
+        foreach ($bind_variables as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+        $stmt->execute();
 
-    bind_variables($stmt, $_GET);
-    $stmt->execute();
-
-    if ($method == "GET") {
         //fetch all results
         $fetched = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $result['TYPE'] = $_GET["what"] . "_package";
-        $result['DATA'] = $fetched;
-    } else {
-        $result = set_http_response(202);
+        return $fetched;
+    } catch (PDOException $e) {
+        $result = set_http_response(500);
+        date_default_timezone_set('America/Toronto');
+        error_log(
+            date("Y-m-d h:i:sa") . " : " . $e->getMessage() . "\n",
+            3,
+            "errors.log"
+        );
+        print json_encode($result, JSON_PRETTY_PRINT);
+        exit();
     }
-
-    echo json_encode($result);
-} catch (PDOException $e) {
-    $result = set_http_response(500);
-    date_default_timezone_set('America/Toronto');
-    error_log(
-        date("Y-m-d h:i:sa") . " : " . $e->getMessage() . "\n",
-        3,
-        "errors.log"
-    );
-    print json_encode($result, JSON_PRETTY_PRINT);
-    exit();
 }
 
 /**
@@ -80,16 +78,53 @@ try {
  */
 function handle_get($parameters)
 {
+    $bind_variables = [];
+    $role = [];
+    if (isset($parameters['user_id'])) {
+        $bind_variables[':user_id'] = $parameters['user_id'];
+        $role = get_query_result(gen_query_user_role(), $bind_variables);
+    }
+    if (isset($parameters['survey_id'])) {
+        $bind_variables[':survey_id'] = $parameters['survey_id'];
+    }
+    if (isset($parameters["term"])) {
+        $bind_variables[':term'] = $parameters["term"];
+    }
+    if (isset($parameters["course_code"])) {
+        $bind_variables[':course_code'] = $parameters["course_code"];
+    }
     switch ($parameters["what"]) {
         case "tas":
-            return set_parameters(true, $parameters);
-
+            if (!isset($parameters["column"])) {
+                $ta = 'ta';
+                $parameters['column'] = $ta;
+            }
+            $tas = get_query_result(
+                set_parameters(true, $parameters),
+                $bind_variables
+            );
+            $ta_package = array('TYPE' => "ta_package", 'DATA' => $tas);
+            return $ta_package;
         case "questions":
-            return gen_query_questions();
-
+            $questions = get_query_result(
+                gen_query_questions(),
+                $bind_variables
+            );
+            $question_package = array(
+                'TYPE' => "questions_package",
+                'DATA' => $questions
+            );
+            return $question_package;
         case "course_pairings":
-            return set_parameters(false, $parameters);
-
+            $course_pairings = get_query_result(
+                set_parameters(false, $parameters),
+                $bind_variables
+            );
+            $course_pairings_package = array(
+                'TYPE' => "course_pairings_package",
+                'DATA' => $course_pairings
+            );
+            return $course_pairings_package;
         case "user_info":
             $include_photo = true;
             if (isset($parameters["include_photo"])) {
@@ -98,13 +133,97 @@ function handle_get($parameters)
                 }
             }
             $list_of_users = is_list_of_users($parameters["user_id"]);
-            return gen_query_user_info($include_photo, $list_of_users);
+            $user_info = get_query_result(
+                gen_query_user_info($include_photo, $list_of_users),
+                $bind_variables
+            );
+            $user_package = array(
+                'TYPE' => "user package",
+                'DATA' => $user_info
+            );
+            return $user_package;
+        case "surveys":
+            $provided_survey = false;
+            if (isset($parameters["survey_id"])) {
+                $provided_survey = $parameters["survey_id"];
+            }
+            $survey_package = get_list_of_surveys(
+                $role[0],
+                $provided_survey,
+                $bind_variables,
+                false
+            );
 
+            return $survey_package;
+        case "survey_results":
+            $provided_survey = false;
+            if (isset($parameters["survey_id"])) {
+                $provided_survey = $parameters["survey_id"];
+            }
+            $survey_package = get_list_of_surveys(
+                $role[0],
+                $provided_survey,
+                $bind_variables,
+                true
+            );
+            return $survey_package;
         default:
             throw new Exception("InvalidPage");
+            break;
     }
 }
 
+/**
+ * Returns true if user_id is a list of users.
+ * @param user_id Value of user_id parameters
+ * @return TRUE if user_id is a list
+ */
+function get_list_of_surveys($role, $survey_id, $bind_variables, $is_instance)
+{
+    $course = false;
+    if (isset($bind_variables[":course_code"])) {
+        $course = true;
+    }
+    $term = false;
+    if (isset($bind_variables[":term"])) {
+        $term = true;
+    }
+    $survey_choices = get_query_result(
+        gen_query_surveys($role, $survey_id, $is_instance, $term, $course),
+        $bind_variables
+    );
+    $list_of_quesitons = get_query_result(gen_query_questions(), []);
+    if (sizeof($survey_choices) <= 0) {
+        $survey_data = null;
+    } elseif ($survey_id) {
+        $survey_data = $survey_choices[0];
+        $survey_data["questions"] = [];
+        if (!isset($survey_data["course_number_locked"])) {
+            $survey_data["course_number_locked"] = null;
+        }
+        $responses = [];
+        if ($is_instance) {
+            $responses = get_query_result(gen_query_survey_responses(), [
+                ":survey_id" => $bind_variables[":survey_id"]
+            ]);
+        }
+        for ($i = 1; $i <= 6; $i++) {
+            $question = $list_of_quesitons[$survey_data["choice" . $i] - 1];
+            $question["responses"] = isset(
+                $responses[$survey_data["choice" . $i] - 1]
+            )
+                ? $responses[$survey_data["choice" . $i] - 1]["answers"]
+                : null;
+            $question['position'] = $i;
+            array_push($survey_data["questions"], $question);
+            unset($survey_data["choice" . $i]);
+        }
+    } else {
+        $survey_data = $survey_choices;
+    }
+    $survey_package = array('TYPE' => "survey_package", 'DATA' => $survey_data);
+    return $survey_package;
+}
 /**
  * Returns true if user_id is a list of users.
  * @param user_id Value of user_id parameters
@@ -118,31 +237,7 @@ function is_list_of_users($user_id)
     if (sizeof($user_id_list) > 1) {
         $list_of_users = true;
     }
-
     return $list_of_users;
-}
-
-/**
- * Binds variables to SQL query based on what parameter
- *
- * @param stmt Reference to PDO object that is a statement to be executed.
- * @param parameters Array of parameters and values
- */
-function bind_variables(&$stmt, $parameters)
-{
-    $stmt->bindParam(':user_id', $parameters["user_id"]);
-    if (
-        ($parameters["what"] == "course_pairings") ||
-        ($parameters["what"] == "tas")
-    ) {
-        if (isset($parameters["term"])) {
-            $stmt->bindParam(':term', $parameters["term"]);
-        }
-
-        if (isset($parameters["course_code"])) {
-            $stmt->bindParam(':course_code', $parameters["course_code"]);
-        }
-    }
 }
 
 /**
@@ -154,6 +249,14 @@ function bind_variables(&$stmt, $parameters)
  */
 function set_parameters($ta_list, $parameters)
 {
+    if ($ta_list) {
+        $is_ta = true;
+        if ($parameters["what"] == "course_pairings") {
+            if ($parameters["column"] == "instructor") {
+                $is_ta = false;
+            }
+        }
+    }
     $course_code = false;
     if (isset($parameters["course_code"])) {
         $course_code = true;
@@ -164,12 +267,6 @@ function set_parameters($ta_list, $parameters)
         $term = true;
     }
 
-    $is_ta = true;
-    if ($parameters["course_code"] == "course_pairings") {
-        if ($parameters["column"] == "instructor") {
-            $is_ta = false;
-        }
-    }
     return gen_query_course_associations($course_code, $term, $is_ta);
 }
 
--- get_query_generators.php
+++ get_query_generators.php
@@ -15,6 +15,32 @@ function gen_query_questions()
 }
 
 /**
+ * Returns sql query that returns an unordered list of questions.
+ *
+ * @return String A sql command that returns an unorderedlist of questions.
+ */
+function gen_query_user_role()
+{
+    return "SELECT CASE WHEN is_admin = 1 THEN 'TRUE' ELSE 'FALSE' END AS is_admin,
+    CASE WHEN is_instructor = 1 THEN 'TRUE' ELSE 'FALSE' END AS is_instructor,
+    CASE WHEN is_ta = 1 THEN 'TRUE' ELSE 'FALSE' END AS is_ta
+    FROM users WHERE user_id = :user_id;";
+}
+
+/**
+ * Returns sql query that returns an unordered list of questions.
+ *
+ * @return String A sql command that returns an unorderedlist of questions.
+ */
+function gen_query_survey_responses()
+{
+    return "SELECT question_id, GROUP_CONCAT(answer) as answers, survey_instance_id
+    FROM responses
+    WHERE survey_instance_id = :survey_id
+    GROUP BY question_id;";
+}
+
+/**
  * Returns sql query that returns an unordered list of tas or instructors.
  * If $ta_list is true, a list of tas is returned. If $ta_list is false,
  * the returned list depends on column parameter in the url.
@@ -95,7 +121,7 @@ function gen_query_course_associations($course_code, $term, $is_ta)
     $return_query =
         "SELECT DISTINCT t1.user_id, t1.course_code, t1.name, t2.term, t2.section_code, " .
         "t1.department_name FROM ($instructor_for_courses) t1 JOIN ($sections) t2 " .
-        "ON t1.section_id=t2.section_id";
+        "ON t1.section_id=t2.section_id;";
 
     return $return_query;
 }
@@ -117,4 +143,140 @@ function gen_query_user_info($include_photo, $list_of_users)
         return "SELECT users.user_id, users.name$photo FROM users WHERE FIND_IN_SET(users.user_id, :user_id);";
     }
     return "SELECT users.user_id, users.name$photo FROM users WHERE users.user_id=:user_id;";
+}
+
+/**
+ * Returns sql query to get data for survey results.
+ * If no survey is select, then it returns all the results available for the
+ * selected ta.
+ *
+ * @return String A sql command to get the requested data
+ */
+function gen_query_surveys($role, $survey_id, $result, $term, $course)
+{
+    //specifications for courses and term
+    $specify_course = "";
+    if ($course) {
+        $specify_course = "WHERE course_code=:course_code";
+    }
+    $specify_term = "";
+    if ($term) {
+        $specify_term = "WHERE surveys.term=:term";
+    }
+    $courses = "SELECT user_id FROM user_associations $specify_course";
+
+    //if no specific survey_id is selected
+    if (!$survey_id) {
+        $user_to_survey = "";
+        // get associated survey to user based on their role
+        if ($role["is_admin"] == "TRUE") {
+            $user_to_survey = "SELECT users.user_id,survey_id, choices_id
+             FROM users JOIN dept_survey_choices JOIN ($courses) c
+             ON users.user_id = dept_survey_choices.user_id
+             AND c.user_id = users.user_id
+             WHERE users.user_id = :user_id";
+        } elseif ($role["is_instructor"] == "TRUE") {
+            if ($course) {
+                $specify_course = "AND course_code=:course_code";
+            }
+            $user_to_survey = "SELECT users.user_id, survey_id, choices_id
+             FROM users JOIN course_survey_choices
+             ON users.user_id = course_survey_choices.user_id
+             WHERE users.user_id = :user_id $specify_course";
+        } elseif ($role["is_ta"] == "TRUE") {
+            $user_to_survey = "SELECT users.user_id, survey_id, choices_id, section_id
+             FROM users JOIN ta_survey_choices JOIN ($courses) c
+             ON users.user_id = ta_survey_choices.user_id
+             AND c.user_id = users.user_id
+             WHERE users.user_id = :user_id";
+        } else {
+            $error = "invalid role";
+            throw new Exception($error);
+        }
+
+        //return list of surveys
+        $survey_package = "SELECT DISTINCT user_to_survey.survey_id, surveys.name
+                             FROM ($user_to_survey) user_to_survey JOIN surveys
+                             ON user_to_survey.survey_id = surveys.survey_id
+                             $specify_term";
+        //if survey_results are requested
+        if ($result) {
+            //get survey_instances
+            $survey_package = "SELECT DISTINCT survey_instances.survey_instance_id, surveys.name
+                             FROM ($user_to_survey) user_to_survey JOIN (surveys,survey_instances)
+                             ON user_to_survey.survey_id = surveys.survey_id
+                             AND survey_instances.survey_id = surveys.survey_id
+                             $specify_term";
+        }
+        return $survey_package;
+    }
+
+    //relate surveys to eachother with the parent attribute
+    $survey_relationships = "SELECT surveys.survey_id as ta_survey_id,
+     t2.survey_id as course_survey_id, t3.survey_id as dept_survey_id
+     FROM surveys JOIN (surveys as t2, surveys as t3)
+     ON surveys.parent_survey = t2.survey_id AND t2.parent_survey = t3.survey_id
+     $specify_term";
+
+    //get the locked columns required by each role
+    $locked_questions = "";
+    $locked_columns = "course_number_locked,dept_number_locked";
+    if ($role["is_admin"] == "TRUE") {
+        $locked_questions = "SELECT DISTINCT dept_survey_id as survey_id, choices_id,
+        dept_survey_choices.number_locked as dept_number_locked
+          FROM ($survey_relationships) survey_relationships JOIN dept_survey_choices
+          ON dept_survey_id = dept_survey_choices.survey_id
+          WHERE dept_survey_id = :survey_id AND dept_survey_choices.user_id = :user_id";
+        $locked_columns = "dept_number_locked";
+    } elseif ($role["is_instructor"] == "TRUE") {
+        $locked_questions = "SELECT DISTINCT course_survey_id as survey_id,
+        course_survey_choices.choices_id, course_survey_choices.number_locked as
+        course_number_locked, dept_survey_choices.number_locked as dept_number_locked
+          FROM ($survey_relationships) survey_relationships JOIN (course_survey_choices,dept_survey_choices)
+          ON course_survey_id = course_survey_choices.survey_id AND dept_survey_id = dept_survey_choices.survey_id
+          WHERE course_survey_id = :survey_id AND course_survey_choices.user_id = :user_id";
+    } elseif ($role["is_ta"] == "TRUE") {
+        $locked_questions = "SELECT ta_survey_id as survey_id, ta_survey_choices.choices_id,
+        course_survey_choices.number_locked as course_number_locked, dept_survey_choices.number_locked as dept_number_locked
+          FROM ($survey_relationships) survey_relationships JOIN (ta_survey_choices, course_survey_choices,dept_survey_choices)
+          ON course_survey_id = course_survey_choices.survey_id AND dept_survey_id = dept_survey_choices.survey_id
+          WHERE ta_survey_id = :survey_id AND ta_survey_choices.user_id = :user_id";
+    } else {
+        $error = "invalid role";
+        throw new Exception($error);
+    }
+
+    // common columns for survey and survey packages
+    $question_columns =
+        " choice1, choice2, choice3, choice4,
+     choice5, choice6, surveys.default_survey_open as start_time, surveys.default_survey_close as end_time ";
+
+    // return survey info and question choices of a particular survey
+    $survey_package = "SELECT surveys.survey_id, surveys.name, $locked_columns, $question_columns
+     FROM ($locked_questions) locked_questions JOIN (choices,surveys)
+     ON locked_questions.choices_id = choices.choices_id AND surveys.survey_id = locked_questions.survey_id;";
+
+    //specified course
+    $course = "SELECT user_association_id FROM user_associations $specify_course";
+
+    //if survey_results are requested
+    if ($result) {
+        //get survey_instances
+        $available_survey_instance = "SELECT survey_instances.survey_instance_id,
+         surveys.name, choices.choices_id, user_association_id, $question_columns
+         FROM survey_instances JOIN (choices,surveys)
+         ON survey_instances.choices_id = choices.choices_id
+         AND surveys.survey_id = survey_instances.survey_id
+         WHERE survey_instance_id = :survey_id";
+
+        // Join with specified term and course if provided and user
+        $term = "SELECT survey_id FROM surveys $specify_term ";
+        $course = "SELECT user_association_id FROM user_associations $specify_course";
+        $user = "SELECT t1.user_association_id FROM user_associations JOIN ($course) t1
+         ON user_associations.user_association_id = t1.user_association_id WHERE user_id = :user_id";
+
+        $survey_package = "SELECT * FROM ($available_survey_instance) t1 JOIN ($user) t2 JOIN ($term) t3
+         ON t1.user_association_id = t2.user_association_id AND t3.survey_id = t1.survey_instance_id ;";
+    }
+    return $survey_package;
 }
