ac5545d03d973c63a4560d3b7adf9fabec9da857
Darren <18633059886@163.com>
2018-07-19 17:19:07 -0400

    try2

--- post_info.php
+++ post_info.php
@@ -619,7 +619,7 @@ function set_new_choices($choices_array)
             for ($j = 1; $j <= 6; $j++) {
                 $choice_name = $choice_label[$i] . "_choice" . $j;
                 $choice_number = "choice$j";
-                $bind_variables[$choice_name] = intval($choices_array[$i][
+                $bind_variables[$choice_name] = intval($choices_array["$i"][
                     $choice_number
                 ]);
             }
