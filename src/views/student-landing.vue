ab1530bc5051b9bf821035aec337433403fc3a61
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-07-20 15:46:07 -0400

    Boiler plate code of student landing

---
+++ src/views/student-landing.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import generate_query_string from "./components/generate_query_string";
+
+export default {
+    data: function() {
+        return {
+        };
+    },
+
+    created: function() {
+
+    }
+};
+</script>
