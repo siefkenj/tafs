493f2ae9fc132fcd6baaec0e22e10bec1f7cc1f8
Daniel Chen <chen.daniel2099@gmail.com>
2018-07-26 21:05:07 -0400

    Added survey wrapper component

--- src/views/components/response_components/survey_summary.vue
+++ src/views/components/response_components/survey_summary.vue
@@ -25,25 +25,27 @@
 </style>
 
 <template>
+
 <div v-if="loading">
-  loading
+    loading
 </div>
-<div v-else-if="numerical_data" class="survey-summary" @click="toggle()">
-  {{numerical_data.name}} Total Responses: {{numerical_data.num_responses}}
-  <div class="numerical-question">
-    <div v-for="question in numerical_data.questions" class="numerical-average">
-      Question:{{question.position}}
-      <div class="average">
-        <div class="percentage-bar" v-bind:style="{width: numerical_data.numerical_average/5*100 + '%'}">
-          {{question.numerical_average}}
+<div v-else-if="numerical_data && numerical_data.num_responses !== 0" class="survey-summary" @click="toggle()">
+    {{numerical_data.name}} Total Responses: {{numerical_data.num_responses}}
+    <div class="numerical-question">
+        <div v-for="question in numerical_data.questions" class="numerical-average">
+            Question:{{question.position}}
+            <div class="average">
+                <div class="percentage-bar" v-bind:style="{width: numerical_data.numerical_average/5*100 + '%'}">
+                    {{question.numerical_average}}
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <div v-if="show_response" class="survey-responses">
-    <response :survey="text_data"> </response>
-  </div>
+    <div v-if="show_response" class="survey-responses">
+        <response :survey="text_data"> </response>
+    </div>
 </div>
+
 </template>
 
 <script>
@@ -94,25 +96,34 @@ export default {
                     this.loading = false;
                 });
         },
+        show: function(show) {
+            this.$emit("show", show);
+        },
         // summarizes the results data to be displayed
         get_summary: function(data) {
-            if (!data) {
+            if (
+                !data ||
+                Object.keys(data).length === 0 ||
+                data.questions.length === 0
+            ) {
                 return null;
             }
-            data.num_responses = data.questions.reduce(
-                (previous, key) => previous + key.responses.length,
-                0
-            );
+            data.num_responses = data.questions
+                .filter(el => el.responses)
+                .reduce((previous, key) => previous + key.responses.length, 0);
             //reduce questions to only contain numerical questions and summarize each question
             data.questions = data.questions
                 .filter(el => el.answer_type !== "comment")
                 .map(el => {
+                    if (!el.responses) {
+                        return el;
+                    }
                     el.num_response = el.responses.length;
                     //sum all numerical responses and round to 1 decimal place
                     el.numerical_average =
                         Math.round(
                             el.responses.reduce(
-                                (previous, key) => previous + parseInt(key),
+                                (previous, key) => previous + parseInt(key, 10),
                                 0
                             ) /
                                 el.num_response *
@@ -120,13 +131,21 @@ export default {
                         ) / 10;
                     return el;
                 });
+            //show component if data exist
+            if (data.num_responses && data.num_responses >= 0) {
+                this.show(true);
+            }
             return data;
         },
         toggle: function() {
             this.show_response = !this.show_response;
         },
         get_text_questions: function(data) {
-            if (!data) {
+            if (
+                !data ||
+                Object.keys(data).length === 0 ||
+                data.questions.length === 0
+            ) {
                 return null;
             }
             data.questions = data.questions.filter(
