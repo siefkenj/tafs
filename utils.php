15624de4f20d6ecf6383b2345ca9ba980be19b07
Jason Siefken <siefkenj@gmail.com>
2018-08-02 08:58:50 -0400

    Added php to get course list from FAS

---
+++ utils.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Get an element from an array returning a default
+ * if the element doesn't exist.
+ */
+function array_get($array, $key, $default = null)
+{
+    return isset($array[$key]) ? $array[$key] : $default;
+}
+
+/**
+ * This function returns an HTTP status corresponding to the result of the
+ * current request
+ *
+ * @param num The HTTP status code
+ * @return array containing the HTTP status of request
+ */
+function set_http_response($num)
+{
+    $http = array(
+        200 => 'HTTP/1.1 200 OK',
+        202 => 'HTTP/1.1 202 Accepted',
+        400 => 'HTTP/1.1 400 Bad Request',
+        500 => 'HTTP/1.1 500 Internal Server Error'
+    );
+
+    header($http[$num]);
+
+    return array('CODE' => $num, 'ERROR' => $http[$num]);
+}
