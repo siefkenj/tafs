9ec3d425e7a1b29f12a67e71a003f65bd94b21a8
Daniel Chen <chen.daniel2099@gmail.com>
2018-08-02 11:02:51 -0400

    Added selection of view mode for surveys

--- src/views/components/response_components/response.vue
+++ src/views/components/response_components/response.vue
@@ -1,8 +1,9 @@
-
+<style scoped>
+</style>
 
 <template>
 
-<div>
+<div style="all: initial;">
     <ul v-for="question in survey.questions">
         <Survey :survey="parse_question(question.content)"></Survey>
         <p v-for="response in question.responses">
@@ -18,24 +19,51 @@ import { Survey, Model } from "survey-vue";
 export default {
     name: "response",
     props: ["survey", "view_mode"],
+    data: function() {
+        return {
+            vmode: this.view_mode
+        };
+    },
     methods: {
         //parse the question content and build into a view only question
         parse_question: function(content) {
             let content_object = JSON.parse(content);
-            // change the question type to display the title in html to
-            // avoid displaying inputs for the question
-            let json = {
-                elements: [
-                    {
-                        type: "html",
-                        html: content_object.title
-                    }
-                ]
-            };
+            let json;
+            switch (this.view_mode) {
+                case "student_view":
+                    // change the question type to display the title in html to
+                    // avoid displaying inputs for the question
+                    json = {
+                        elements: [content_object]
+                    };
+                    break;
+                case "question_only":
+                    json = {
+                        elements: [
+                            {
+                                type: "html",
+                                html: content_object.title
+                            }
+                        ]
+                    };
+                    break;
+                default:
+                    console.warn(
+                        this.view_mode,
+                        "is not recognized as a view mode"
+                    );
+            }
+
             let survey = new Model(json);
             // to remove the submit button for survey, set to read-only mode
             survey.mode = "display";
+            survey.showQuestionNumbers = "off";
             return survey;
+        }
+    },
+    watch: {
+        view_mode: function() {
+            this.vmode = this.view_mode;
         }
     },
     components: {
