2a33f57126fea66c297ddb6c997d588c493144c8
Daniel Chen <chen.daniel2099@gmail.com>
2018-08-10 13:20:55 -0400

    new sample data and fixed API tests that were broken

--- test/back_end/api_student_survey_test.spec.js
+++ test/back_end/api_student_survey_test.spec.js
@@ -8,11 +8,11 @@ const base64 = base64o.base64;
 import generate_query_string from "../../src/views/components/generate_query_string.js";
 
 describe("Test Student Survey API", function() {
-    it("Should return TA info for override token HLF43W", async function() {
+    it("Should return TA info for override token BZ2RSR", async function() {
         let query_string = generate_query_string({
             what: "get_ta",
             user_id: "student1",
-            override_token: "HLF43W"
+            override_token: "BZ2RSR"
         });
         let fetched = await fetch(
             "http://localhost:3000/student_survey.php?" + query_string
@@ -24,11 +24,11 @@ describe("Test Student Survey API", function() {
         expect(fetchedJSON.TYPE).to.equal("ta_package");
     });
 
-    it("Should return survey question package for override token HLF43W", async function() {
+    it("Should return survey question package for override token BZ2RSR", async function() {
         let query_string = generate_query_string({
             what: "get_surveys",
             user_id: "student1",
-            override_token: "HLF43W"
+            override_token: "BZ2RSR"
         });
         let fetched = await fetch(
             "http://localhost:3000/student_survey.php?" + query_string
