501b92ad7fe4eaf0f63581316a0f12d6596a77d6
Daniel Chen <chen.daniel2099@gmail.com>
2018-08-16 16:00:25 -0400

    added tests and fixed bug in crafted data

--- db/generate_sample_data.py
+++ db/generate_sample_data.py
@@ -79,7 +79,7 @@ class GenExamples:
         '''INSERT INTO choices(choice1,choice2,choice3,choice4,choice5,choice6) VALUES (6,3,7,5,2,8);''',
         '''INSERT INTO ta_survey_choices (choices_id, section_id, user_id) VALUES (31,163,'Hame23');''',
         '''INSERT INTO surveys(dept_survey_choice_id,course_survey_choice_id,ta_survey_choice_id,name,term,default_survey_open,default_survey_close) VALUES (4,NULL,164,'Crafted Survey','201809','2018-11-09 00:00:00','2018-11-12 00:00:00');''',
-        '''INSERT INTO survey_instances(viewable_by_others,survey_id,choices_id,user_association_id,override_token,survey_open,survey_close,name) VALUES (0,101,32,814,'BZ2RSR','2018-11-09 00:00:00','2018-11-12 00:00:00','Crafted Survey');''',
+        '''INSERT INTO survey_instances(viewable_by_others,survey_id,choices_id,user_association_id,override_token,survey_open,survey_close,name) VALUES (0,101,32,814,'SD1B61','2018-11-09 00:00:00','2018-11-12 00:00:00','Crafted Survey');''',
         '''INSERT INTO responses(survey_instance_id,question_id,answer,user_id) VALUES (200,1,'5','allen52');''',
         '''INSERT INTO responses(survey_instance_id,question_id,answer,user_id) VALUES (200,1,'1','ramire63');''',
         '''INSERT INTO responses(survey_instance_id,question_id,answer,user_id) VALUES (200,1,'1','hunter79');''',
